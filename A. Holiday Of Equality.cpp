75dc935fa9811d69accc44bff57630ad494ed123
SohagMollik <74552043+SohagMollik@users.noreply.github.com>
2021-02-18 19:52:34 +0600

    Add files via upload

--- A. Holiday Of Equality.cpp
+++ A. Holiday Of Equality.cpp
@@ -1,29 +1,18 @@
 #include<bits/stdc++.h>
-#define ll long long
 using namespace std;
-set<ll>s;
-set<ll>::iterator it;
 int main()
 {
-    ll n;
-    cin>>n;
-    ll a[n];
-    for(ll i=0;i<n;i++){
-        cin>>a[i];
-    }
-
-    if(n==1)cout<<0<<endl;
-    else{
-        for(ll i=0;i<n;i++){
-          int x=a[i];
-          s.insert(x);
-        }
-
-    ll sum=0;
-    for(it=s.begin();it!=s.end();it++){
-        sum+=*it;
-    }
-    cout<<sum<<endl;
-    }
+   int n;
+   cin>>n;
+   int a[n];
+   for(int i=0;i<n;i++){
+    cin>>a[i];
+   }
+   sort(a,a+n,greater<int>());
+   int sum=0;
+   //cout<<a[0]<<endl;
+   for(int i=0;i<n;i++){
+    sum+=a[0]-a[i];
+   }
+   cout<<sum<<endl;
 }
-
