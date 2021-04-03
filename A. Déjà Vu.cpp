dffeff59a4b4c3827efe5a4047fd6b571ec800ce
SohagMollik <74552043+SohagMollik@users.noreply.github.com>
2021-04-04 01:36:43 +0600

    Add files via upload

---
+++ A. Déjà Vu.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+   int t;
+   cin>>t;
+   while(t--){
+   string s,s1="a",s3;
+   cin>>s;
+   s3=s+s1;
+   reverse(s3.begin(),s3.end());
+   if(s3!=s+s1){
+    cout<<"YES\n";
+    cout<<s3<<endl;
+   }
+   else{
+    s3=s1+s;
+    reverse(s3.begin(),s3.end());
+    if(s3!=s1+s){
+        cout<<"YES\n";
+        cout<<s3<<endl;
+    }
+    else cout<<"NO\n";
+   }
+   }
+   return 0;
+}
