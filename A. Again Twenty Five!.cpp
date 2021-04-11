82679665d0047aa9b04dad12df8adac6b4163bc1
SohagMollik <74552043+SohagMollik@users.noreply.github.com>
2021-04-12 01:27:58 +0600

    Add files via upload

---
+++ A. Again Twenty Five!.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll n;
+    cin>>n;
+    cout<<25<<endl;
+    return 0;
+}
