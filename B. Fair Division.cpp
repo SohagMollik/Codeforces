b13b5e7d988fe579ba3f42323f3075e2337e46d7
SohagMollik <74552043+SohagMollik@users.noreply.github.com>
2021-05-06 00:29:47 +0600

    Add files via upload

--- B. Fair Division.cpp
+++ B. Fair Division.cpp
@@ -2,36 +2,41 @@
 using namespace std;
 int main()
 {
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        int a[n];
-        for(int i=0;i<n;i++){
-            cin>>a[i];
-        }
-        if(n%2==0){
-        int sum1=0,sum2=0;
-        for(int i=0;i<n/2;i++){
-            sum1+=a[i];
-        }
-        for(int i=(n/2);i<n;i++){
-            sum2+=a[i];
-        }
-        if(sum1==sum2)cout<<"YES\n";
-        else cout<<"NO\n";
-        }
-        else{
-               int sum3=0,sum4=0;
-            for(int i=0;i<(n/2)+1;i++){
-                sum3+=a[i];
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+    int a[n],sum=0;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        sum+=a[i];
+    }
+    int f=1;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(a[i]!=a[j]){
+                f=0;
             }
-            for(int i=(n/2)+1;i<n;i++){
-                sum4+=a[i];
-            }
-        if(sum3==sum4)cout<<"YES\n";
-        else cout<<"NO\n";
         }
     }
+    if(f==1)
+    {
+        if(n%2==0)
+        cout<<"YES\n";
+        else
+        cout<<"NO\n";
+
+    }
+    else{
+        if(sum%2!=0)
+        cout<<"NO\n";
+        else
+        cout<<"YES\n";
+    }
+  }
+  return 0;
+
 }
