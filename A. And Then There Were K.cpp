90c3c8d14d11122be36b81fde9f9aab43f00e521
SohagMollik <sohagmollik871@gmail.com>
2021-05-21 14:23:41 +0600

    Add files via upload

---
+++ A. And Then There Were K.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define ll    long long
+#define pb    push_back
+#define ull   unsigned long long
+#define f     first
+#define s     second
+#define mod   1000000007
+#define pi    acos(-1.0)
+#define fast  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main()
+{
+    fast;
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int x=log2(n);
+        int y=1<<x;
+        y-=1;
+        cout<<y<<endl;
+
+    }
+
+    return 0;
+}
