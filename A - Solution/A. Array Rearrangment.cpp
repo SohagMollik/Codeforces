1130707601ec5ed7c653f05f19576f27b5046bc9
Sohag Mollik <sohagmollik871@gmail.com>
2022-02-12 22:54:26 +0600

    Update A. Array Rearrangment.cpp

--- A - Solution/A. Array Rearrangment.cpp
+++ A - Solution/A. Array Rearrangment.cpp
@@ -1,26 +1,33 @@
 #include<bits/stdc++.h>
+#define ll    long long
+#define pb    push_back
+#define ull   unsigned long long
+#define f     first
+#define s     second
+#define mod   1000000007
+#define pi    acos(-1.0)
+#define fast  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
 int main()
 {
+    fast;
     int t;
     cin>>t;
     while(t--)
     {
-        int n,x;
+        int n,x,ans=0;
         cin>>n>>x;
         int a[n],b[n];
-        for(int i=0;i<n;i++){
-            cin>>a[i];
-        }
-        for(int i=0;i<n;i++){
-            cin>>b[i];
-        }
+        for(int i=0;i<n;i++)cin>>a[i];
+        for(int i=0;i<n;i++)cin>>b[i];
+        sort(a,a+n);
         sort(b,b+n,greater<int>());
-        bool t=false;
         for(int i=0;i<n;i++){
-            if(a[i]+b[i]<=x)t=true;
+            if(a[i]+b[i]<=x)ans++;
         }
-        if(t==true)cout<<"YES\n";
-        else cout<<"NO\n";
+        if(ans==n)cout<<"Yes\n";
+        else cout<<"No\n";
     }
+
+    return 0;
 }
