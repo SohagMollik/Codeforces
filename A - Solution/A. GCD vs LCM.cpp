777b0148065a316e7802159b0b49952c1c52cfff
Sohag Mollik <sohagmollik871@gmail.com>
2022-04-09 01:21:00 +0600

    Add files via upload

---
+++ A - Solution/A. GCD vs LCM.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define nn "\n"
+#define fast  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main()
+{
+   fast;
+   int t;
+   cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+         cout<<n-3<<" "<<1<<" "<<1<<" "<<1<<nn;
+    }
+    return 0;
+}
