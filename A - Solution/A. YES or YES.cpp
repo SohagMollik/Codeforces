d7bcff3efb8c2c7ea8790543ac305a494d44423f
Sohag Mollik <sohagmollik871@gmail.com>
2022-07-18 15:35:20 +0600

    Add files via upload

---
+++ A - Solution/A. YES or YES.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define ll   long long int
+#define pb   push_back
+#define ull  unsigned long long
+#define f    first
+#define s    second
+#define mod  1000000007
+#define nn   "\n"
+#define yes  printf("YES\n");
+#define no   printf("NO\n");
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        if((s[0]=='y'||s[0]=='Y')&&(s[1]=='e' || s[1]=='E')&&(s[2]=='s' || s[2]=='S'))yes
+        else no
+    }
+
+    return 0;
+}
+
