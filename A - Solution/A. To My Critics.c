ee817c0f657a35d6980f749cd9da860b5f83a340
Sohag Mollik <sohagmollik871@gmail.com>
2023-07-22 00:00:26 +0600

    Add files via upload

---
+++ A - Solution/A. To My Critics.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b,c;
+        scanf("%d%d%d",&a,&b,&c);
+        if(a+b>=10)printf("YES\n");
+        else if(b+c>=10)printf("YES\n");
+        else if(a+c>=10)printf("YES\n");
+        else printf("NO\n");
+    }
+    return 0;
+}
